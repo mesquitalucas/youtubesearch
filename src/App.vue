08f5f4359d7004d1998c57a0eca59a66995e78c2
mesquitalucas <lucas.mesquita@al.resilia.com.br>
2020-08-03 07:58:32 -0300

    calopsita

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <SearchForm v-on:search="search"/>
+    <SearchResults
+      v-if="videos.length > 0"
+      :videos="videos"
+      :reformattedSearchString="reformattedSearchString"
+    />
+    <Pagination
+      v-if="videos.length > 0"
+      :prevPageToken="api.prevPageToken"
+      :nextPageToken="api.nextPageToken"
+      v-on:prev-page="prevPage"
+      v-on:next-page="nextPage"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import Header from './components/layout/Header';
+  import SearchForm from './components/SearchForm';
+  import SearchResults from './components/SearchResults';
+  import Pagination from './components/Pagination';
+  import axios from 'axios';
+
+  export default {
+    name: 'app',
+    components: {
+      Header,
+      SearchForm,
+      SearchResults,
+      Pagination
+    },
+
+    data() {
+      return {
+        videos: [],
+        reformattedSearchString: '',
+        api: {
+          baseUrl: 'https://www.googleapis.com/youtube/v3/search?',
+          part: 'snippet',
+          type: 'video',
+          order: 'viewCount',
+          maxResults: 21,
+          q: '',
+          key: 'AIzaSyAW0BUoRWL-WmaoXhQLeHPCmBeP3Er2MyI',
+          prevPageToken: '',
+          nextPageToken: ''
+        }
+      };
+    },
+
+    methods: {
+      search(searchParams) {
+        this.reformattedSearchString = searchParams.join(' ');
+        this.api.q = searchParams.join('+');
+        const { baseUrl, part, type, order, maxResults, q, key } = this.api;
+        const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}`;
+        this.getData(apiUrl);
+      },
+
+      prevPage() {
+        const { baseUrl, part, type, order, maxResults, q, key, prevPageToken } = this.api;
+        const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${prevPageToken}`;
+        this.getData(apiUrl);
+      },
+
+      nextPage() {
+        const { baseUrl, part, type, order, maxResults, q, key, nextPageToken } = this.api;
+        const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${nextPageToken}`;
+        this.getData(apiUrl);
+      },
+
+      getData(apiUrl) {
+        axios
+          .get(apiUrl)
+          .then(res => {
+            this.videos = res.data.items;
+            this.api.prevPageToken = res.data.prevPageToken;
+            this.api.nextPageToken = res.data.nextPageToken;
+          })
+          .catch(error => console.log(error));
+      }
+    }
+  };
+
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
